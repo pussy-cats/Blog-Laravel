d7e66d4f30e23d4377910e0f5d53fe3b9e781abb
pussy-cats <amardikamahdi@gmail.com>
2020-05-21 01:50:31 +0700

    making table structure for Post Table

--- database/migrations/2020_05_20_184108_create_posts_table.php
+++ database/migrations/2020_05_20_184108_create_posts_table.php
@@ -15,7 +15,10 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
